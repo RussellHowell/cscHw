7155c57b59034a465359e6078c187c7e68ce7c74
narciero <narciero@me.com>
2015-02-19 18:55:36 -0500

    update

--- DatabasedScript.sql
+++ DatabasedScript.sql
@@ -6,6 +6,35 @@ CREATE TABLE CLIENT
 	PhoneNumber varchar(10) NULL,
 	PRIMARY KEY (ClientId)
 );
+CREATE TABLE DEPARTMENT
+(
+	DepartmentId varchar(10) NOT NULL,
+	DepartmentName varchar(30) NULL,
+	NumberOfEmployees int NOT NULL,
+	PhoneNumber varchar(10) NOT NULL,
+	ManagerId varchar(10) NOT NULL,
+	PRIMARY KEY (DepartmentId)
+);
+CREATE TABLE EMPLOYEE
+(
+	EmployeeId varchar(10) NOT NULL,
+	EmployeeSsn varchar(9) NOT NULL,
+	FirstName varchar(20) NOT NULL,
+	MiddleName varchar(20) NULL,
+	LastName varchar(20) NOT NULL,
+	Address varchar(50) NOT NULL,
+	Salary decimal(13,2) NOT NULL,
+	PhoneNumber varchar(10) NULL,
+	Birthdate date NOT NULL,
+	StartDate date NOT NULL,
+	Sex varchar(1) NOT NULL,
+	SupervisorId varchar(10) NULL,
+	DepartmentId varchar(10) NOT NULL,
+	PRIMARY KEY (EmployeeId),
+	CONSTRAINT fk_supervisor_id FOREIGN KEY (SupervisorId) REFERENCES EMPLOYEE(EmployeeId),
+	CONSTRAINT fk_department_id FOREIGN KEY (DepartmentId) REFERENCES DEPARTMENT(DepartmentId)
+);
+ALTER TABLE DEPARTMENT ADD CONSTRAINT fk_manager_id FOREIGN KEY (ManagerId) REFERENCES EMPLOYEE(EmployeeId);
 CREATE TABLE ACCOUNT_TYPE
 (
 	AccountTypeId varchar(10) NOT NULL,
@@ -22,12 +51,19 @@ CREATE TABLE ACCOUNT
 	PRIMARY KEY (AccountId),
 	CONSTRAINT fk_account_type FOREIGN KEY (AccountType) REFERENCES ACCOUNT_TYPE(AccountTypeId)
 );
+CREATE TABLE PORTFOLIO
+(
+	PortfolioId varchar(10) NOT NULL,
+	PortfolioBalance decimal(13,2) NOT NULL,
+	SetupDate timestamp(0) NOT NULL,
+	PRIMARY KEY (PortfolioId)
+);
 CREATE TABLE MUTUAL_FUND_TYPE
 (
 	MutualFundTypeId varchar(10) NOT NULL,
-	MutualFundTypeDescription varchar(30) NOT NULL,
+	MutualFundTypeDescription varchar(30) NULL,
 	PRIMARY KEY (MutualFundTypeId)
-)
+);
 CREATE TABLE MUTUAL_FUND
 (
 	MutualFundId varchar(10) NOT NULL,
@@ -39,14 +75,14 @@ CREATE TABLE MUTUAL_FUND
 CREATE TABLE STOCK_TYPE
 (
 	StockTypeId varchar(10) NOT NULL,
-	StockTypeDescription varchar(30) NOT NULL,
+	StockTypeDescription varchar(30) NULL,
 	PRIMARY KEY (StockTypeId)
 );
 CREATE TABLE STOCK
 (
 	StockId varchar(6) NOT NULL,
 	StockType varchar(10) NOT NULL,
-	StockName varchar(30) NOT NULL,
+	StockName varchar(30) NULL,
 	StockExchange varchar(10) NOT NULL,
 	PRIMARY KEY (StockId),
 	CONSTRAINT fk_stock_type FOREIGN KEY (StockType) REFERENCES STOCK_TYPE (StockTypeId)
@@ -54,9 +90,9 @@ CREATE TABLE STOCK
 CREATE TABLE BOND_TYPE
 (
 	BondTypeId varchar(10) NOT NULL,
-	BondTypeDescription varchar(30) NOT NULL,
-	PRIMARY (BondTypeId)
-);;
+	BondTypeDescription varchar(30) NULL,
+	PRIMARY KEY (BondTypeId)
+);
 CREATE TABLE BOND
 (
 	BondId varchar(12) NOT NULL,
@@ -69,4 +105,18 @@ CREATE TABLE BOND
 	InterestRate decimal(8,4) NOT NULL,
 	PRIMARY KEY (BondId),
 	CONSTRAINT fk_bond_type FOREIGN KEY (BondType) REFERENCES BOND_TYPE (BondTypeId)
+);
+CREATE TABLE PORTFOLIO_ITEM
+(
+	PortfolioItemId varchar(10) NOT NULL,
+	PortfolioId varchar(10) NOT NULL,
+	MutualFundId varchar(10) NULL,
+	StockId varchar(6) NULL,
+	BondId varchar(12) NULL,
+	Quantity integer NOT NULL,
+	PRIMARY KEY (PortfolioItemId, PortfolioId),
+	CONSTRAINT fk_portfolio_id FOREIGN KEY (PortfolioId) REFERENCES PORTFOLIO(PortfolioId),
+	CONSTRAINT fk_mutual_fund_id FOREIGN KEY (MutualFundId) REFERENCES MUTUAL_FUND(MutualFundId),
+	CONSTRAINT fk_stock_id FOREIGN KEY (StockId) REFERENCES STOCK(StockId),
+	CONSTRAINT fk_bond_id FOREIGN KEY (BondId) REFERENCES BOND(BondId)
 );
